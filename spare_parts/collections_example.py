10e3beb49215a94ec462f60ae9d496c7ba6516bc
Matt <Matt@Admins-MacBook-Air.local>
2020-11-10 14:11:14 -0500

    more

--- spare_parts/collections_example.py
+++ spare_parts/collections_example.py
@@ -17,3 +17,28 @@ KeyPress = collections.namedtuple('KeyPress', ('inputtype', 'buttontopress'))
 m1b1 = KeyPress('KEYBOARD', 'KeycodeA')
 
 print(m1b1.inputtype)
+
+import pprint
+
+button_list = []
+
+for m in range(1,6):
+    for b in range(1,6):
+        button_list.append('m' + str(m) + 'b' + str(b))
+
+print(button_list)
+print(len(button_list))
+
+fun_list = []
+
+KeyPress = collections.namedtuple('KeyPress', ('inputtype', 'buttontopress'))
+
+for f in range(0,25):
+    fun_list.append(KeyPress('KEYBOARD', 'KeycodeA'))
+print(len(fun_list))
+
+lst_tuple = list(zip(button_list,fun_list))
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(lst_tuple)
+
+print(len(lst_tuple))
