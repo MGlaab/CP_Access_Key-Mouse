cc865780f4c5a40f2339ee03b564653641082d61
Matt <Matt@Admins-MacBook-Air.local>
2020-11-17 08:37:10 -0500

    more

--- revisions/split_code.py
+++ revisions/split_code.py
@@ -4,9 +4,8 @@ import digitalio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
-from adafruit_hid.mouse import Mouse
 import neopixel
+# new imports
 import key_functions
 import file_to_change
 
